efa5eb0d9c4dccb9ac84836bfc1ec5a74f4d5583
agonzalez@adexus.cl <agonzalez@adexus.cl>
2020-01-02 18:02:14 -0330

    Lección #03 y #04 webservice spriingboot

---
+++ spring-restful-webservices/src/main/java/com/example/springrestfulwebservices/SpringRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulWebservicesApplication.class, args);
+	}
+
+}
