5aa4b422a59e96f7c930869d9ebfa611a9489cf4
Angel <angelgonzalez1594@gmail.com>
2020-01-12 21:17:21 -0300

    Step : 36/37

--- spring-restful-webservices/src/main/java/com/example/springrestfulwebservices/SwaggerConfig.java
+++ spring-restful-webservices/src/main/java/com/example/springrestfulwebservices/SwaggerConfig.java
@@ -1,8 +1,16 @@
 package com.example.springrestfulwebservices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,9 +19,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	// En este punto podemos personalizar la documentacion
+	public static final Contact DEFAULT_CONTACT = new Contact("Angel Gonzalez", "www.angelgonzalez.com", "angelgonzalez1594@gmail.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("My First Api Documentation", "The Api Documentation", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+	private static final Set<String> DEFAULT_PRODUCES = new HashSet<String>(Arrays.asList("aplication/json","aplication/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES)
+				.consumes(DEFAULT_PRODUCES);//Informacion general del api;
 	}
 
 }
--- spring-restful-webservices/src/main/java/com/example/springrestfulwebservices/user/User.java
+++ spring-restful-webservices/src/main/java/com/example/springrestfulwebservices/user/User.java
@@ -5,12 +5,18 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@ApiModel(description="All user details") //descripcion para swagger
 public class User {
 
 	private Integer id;
 	@Size(min=2,message="Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Name should have atleast 2 characters") //descripcion para swagger
+
 	private String name;
 	@Past
+	@ApiModelProperty(notes="birth date cannot be in future")
 	private Date birthDate;
 	
 	
